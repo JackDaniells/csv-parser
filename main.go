06cc15eba4b5e22e3921e9bc2d002cd451406ad0
dkock <daniel.kock@mercadolivre.com>
2021-12-09 22:24:38 -0300

    call reader in main

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	reader "rain-csv-parser/src/pkg/reader"
+)
+
+const (
+	INPUT_FOLDER = "input"
+	OUTPUT_FILE  = "output"
+
+	CSV_EXTENSION = "csv"
 )
 
 func run() {
-	fmt.Println("Parser running...")
+	fmt.Println("Initializing parser...")
+
+	readerModule, err := reader.NewReader(INPUT_FOLDER, CSV_EXTENSION)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println("Parser running!")
+
+	fmt.Println(fmt.Sprintf("Reading all files from %s folder", INPUT_FOLDER))
+	readerModule.Read()
+
 }
 
 func main() {
