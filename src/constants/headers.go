b0e791d87b2ef2f584fe619331db69efcece98ff
dkock <daniel.kock@mercadolivre.com>
2021-12-14 09:51:53 -0300

    add constant files

---
+++ src/constants/headers.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	HeaderID        = "id"
+	HeaderFirstName = "first name"
+	HeaderLastName  = "last name"
+	HeaderName      = "name"
+	HeaderEmail     = "e-mail"
+	HeaderSalary    = "salary"
+)
