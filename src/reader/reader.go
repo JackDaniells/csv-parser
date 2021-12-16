8d6c0b38c223a0325e38fdd06ecdcd2e029de0e1
dkock <daniel.kock@mercadolivre.com>
2021-12-15 22:43:25 -0300

    add logs in reader

--- src/reader/reader.go
+++ src/reader/reader.go
@@ -2,6 +2,7 @@ package reader
 
 import (
 	"errors"
+	"fmt"
 	"rain-csv-parser/src/domain"
 	"rain-csv-parser/src/pkg/logger"
 )
@@ -23,15 +24,16 @@ func NewReaderService(ioStrategy IOStrategy) *readerService {
 }
 
 func (reader *readerService) Read(inputPath string) (table *domain.TableDomain, err error) {
-	logger.Debug().Log("Reading input path...")
+	logger.Debug().Log(fmt.Sprintf("Reading input from %s...", inputPath))
 	data, err := reader.ioStrategy.Read(inputPath)
 	if err != nil {
+		logger.Error().Log("Error when read input path:", err.Error())
 		return nil, err
 	}
 
 	if len(data) == 0 {
 		err = errors.New("input table is empty")
-		logger.Error().Log("Error when read data from input path:", err.Error())
+		logger.Error().Log("Error when read input path:", err.Error())
 		return nil, err
 	}
 
